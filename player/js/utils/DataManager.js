354a754cd26bfff0638a19887c94bef13539ae99
= <=>
2015-09-26 17:39:39 -0300

    changing index json

--- player/js/utils/DataManager.js
+++ player/js/utils/DataManager.js
@@ -59,20 +59,20 @@ function dataFunctionManager(){
                 }
                 layerData.tm = timeValues;
             }
-            if(layerData.ks.o.k instanceof Array){
-                convertNumericValue(layerData.ks.o.k,1/100);
+            if(layerData.ks.o instanceof Array){
+                convertNumericValue(layerData.ks.o,1/100);
             }else{
-                layerData.ks.o.k /= 100;
+                layerData.ks.o /= 100;
             }
-            if(layerData.ks.s.k instanceof Array){
-                convertNumericValue(layerData.ks.s.k,1/100);
+            if(layerData.ks.s instanceof Array){
+                convertNumericValue(layerData.ks.s,1/100);
             }else{
-                layerData.ks.s.k /= 100;
+                layerData.ks.s /= 100;
             }
-            if(layerData.ks.r.k instanceof Array){
-                convertNumericValue(layerData.ks.k.r.k,degToRads);
+            if(layerData.ks.r instanceof Array){
+                convertNumericValue(layerData.ks.r,degToRads);
             }else{
-                layerData.ks.r.k *= degToRads;
+                layerData.ks.r *= degToRads;
             }
             if(layerData.hasMask){
                 var maskProps = layerData.masksProperties;
@@ -138,10 +138,10 @@ function dataFunctionManager(){
                 isTrimmed = true;
             }
             if(arr[i].ty == 'fl' || arr[i].ty == 'st'){
-                if(arr[i].o.k instanceof Array){
-                    convertNumericValue(arr[i].o.k,1/100);
+                if(arr[i].o instanceof Array){
+                    convertNumericValue(arr[i].o,1/100);
                 }else{
-                    arr[i].o.k *= 1/100;
+                    arr[i].o *= 1/100;
                 }
             }else if(arr[i].ty == 'sh'){
                 arr[i].trimmed = isTrimmed;
@@ -161,20 +161,20 @@ function dataFunctionManager(){
             }else if(arr[i].ty == 'tr'){
                 transformData = arr[i];
                 transformData.renderedData = [];
-                if(transformData.o.k instanceof Array){
-                    convertNumericValue(transformData.o.k,1/100);
+                if(transformData.o instanceof Array){
+                    convertNumericValue(transformData.o,1/100);
                 }else{
-                    transformData.o.k /= 100;
+                    transformData.o /= 100;
                 }
-                if(transformData.s.k instanceof Array){
-                    convertNumericValue(transformData.s.k,1/100);
+                if(transformData.s instanceof Array){
+                    convertNumericValue(transformData.s,1/100);
                 }else{
-                    transformData.s.k /= 100;
+                    transformData.s /= 100;
                 }
-                if(transformData.r.k instanceof Array){
-                    convertNumericValue(transformData.r.k,degToRads);
+                if(transformData.r instanceof Array){
+                    convertNumericValue(transformData.r,degToRads);
                 }else{
-                    transformData.r.k *= degToRads;
+                    transformData.r *= degToRads;
                 }
             }else if(arr[i].ty == 'rc' || arr[i].ty == 'el'){
                 arr[i].trimmed = isTrimmed;
@@ -208,8 +208,7 @@ function dataFunctionManager(){
         return string;
     }
 
-    function getInterpolatedValue(keys, frameNum, offsetTime,paramArr,arrPos,arrLen){
-        var keyframes = keys.k;
+    function getInterpolatedValue(keyframes, frameNum, offsetTime,paramArr,arrPos,arrLen){
         var keyData, nextKeyData,propertyArray,bezierData;
         var i;
         var len,paramCnt = 0;
@@ -416,7 +415,7 @@ function dataFunctionManager(){
         var isTrimmed = trimData && trimData.length > 0;
         var pathData = {};
         pathData.closed = isMask ? shapeData.cl : shapeData.closed;
-        var keyframes = isMask ? shapeData.pt.k : shapeData.ks.k;
+        var keyframes = isMask ? shapeData.pt : shapeData.ks;
         if(keyframes.v){
             if(!isTrimmed){
                 pathData.pathNodes = keyframes;
