7b7fe8f986622c629391bfd1053e4abef7e47bc9
= <=>
2016-01-14 21:19:01 -0300

    setMatte html placeholder

--- extension/jsx/renderManager.jsx
+++ extension/jsx/renderManager.jsx
@@ -1,5 +1,6 @@
 /*jslint vars: true , plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
-/*global bm_layerElement, bm_eventDispatcher, bm_sourceHelper, bm_generalUtils, bm_compsManager, bm_dataManager, app, File*/
+/*global bm_layerElement, bm_eventDispatcher, bm_sourceHelper, bm_generalUtils, bm_compsManager, bm_downloadManager, bm_textShapeHelper, bm_markerHelper, app, File, bm_dataManager*/
+
 var bm_renderManager = (function () {
     'use strict';
     
@@ -63,6 +64,9 @@ var bm_renderManager = (function () {
             layerData = layers[i];
             layerInfo = comp.layers[i + 1];
             bm_layerElement.checkLayerSource(layerInfo, layerData);
+            if (layerData.ty === bm_layerElement.layerTypes.text) {
+                bm_textShapeHelper.addComps();
+            }
             if (layerData.ty === bm_layerElement.layerTypes.precomp && layerData.render !== false && layerData.compId) {
                 layerData.layers = [];
                 createLayers(layerInfo.source, layerData.layers, framerate);
@@ -70,18 +74,20 @@ var bm_renderManager = (function () {
         }
     }
     
-    function render(comp, destination, settings) {
+    function render(comp, destination, compSettings) {
         currentCompID = comp.id;
-        currentCompSettings = settings;
+        currentCompSettings = compSettings;
         bm_eventDispatcher.sendEvent('bm:render:update', {type: 'update', message: 'Starting Render', compId: currentCompID, progress: 0});
         destinationPath = destination;
         bm_sourceHelper.reset();
+        bm_textShapeHelper.reset();
         pendingLayers.length = 0;
         pendingComps.length = 0;
         var exportData = ob.renderData.exportData;
         exportData.assets = [];
         exportData.comps = [];
-        exportData.v = '3.1.8';
+        exportData.fonts = [];
+        exportData.v = '3.1.6';
         exportData.layers = [];
         exportData.ip = comp.workAreaStart * comp.frameRate;
         exportData.op = (comp.workAreaStart + comp.workAreaDuration) * comp.frameRate;
@@ -106,6 +112,7 @@ var bm_renderManager = (function () {
         bm_dataManager.saveData(ob.renderData.exportData, destinationPath, currentCompSettings);
         bm_eventDispatcher.sendEvent('bm:render:update', {type: 'update', message: 'Render finished ', compId: currentCompID, progress: 1, isFinished: true, fsPath: currentCompSettings.fsName});
         reset();
+        bm_textShapeHelper.removeComps();
         bm_compsManager.renderComplete();
     }
     
@@ -141,12 +148,55 @@ var bm_renderManager = (function () {
         }
     }
     
-    function imagesReady() {
+    function checkFonts() {
+        var fonts = bm_sourceHelper.getFonts();
+        var exportData;
+        if (fonts.length === 0) {
+            saveData();
+        } else {
+            if (currentCompSettings.glyphs) {
+                var fontsInfo = {
+                    list: []
+                };
+                var list = fontsInfo.list;
+                var i, len = fonts.length, fontOb;
+                for (i = 0; i < len; i += 1) {
+                    fontOb = {};
+                    fontOb.fName = fonts[i].name;
+                    fontOb.fFamily = fonts[i].family;
+                    fontOb.fStyle = fonts[i].style;
+                    list.push(fontOb);
+                }
+                exportData = ob.renderData.exportData;
+                exportData.fonts = fontsInfo;
+                bm_textShapeHelper.exportFonts(fontsInfo);
+                bm_textShapeHelper.exportChars(fontsInfo);
+            } else {
+                exportData = ob.renderData.exportData;
+                bm_eventDispatcher.sendEvent('bm:render:fonts', {type: 'save', compId: currentCompID, fonts: fonts});
+            }
+        }
+    }
+    
+    function setChars(chars) {
+        bm_eventDispatcher.sendEvent('bm:render:chars', {type: 'save', compId: currentCompID, chars: chars});
+    }
+    
+    function setFontData(fontData) {
+        var exportData = ob.renderData.exportData;
+        exportData.fonts = fontData;
+        bm_textShapeHelper.exportFonts(fontData);
+        bm_textShapeHelper.exportChars(fontData);
+    }
+    
+    function setCharsData(charData) {
+        var exportData = ob.renderData.exportData;
+        exportData.chars = charData;
         saveData();
     }
     
-    function imagesSaved() {
-        saveData();
+    function imagesReady() {
+        checkFonts();
     }
     
     function renderLayerComplete() {
@@ -161,7 +211,10 @@ var bm_renderManager = (function () {
     ob.render = render;
     ob.renderLayerComplete = renderLayerComplete;
     ob.renderNextLayer = renderNextLayer;
+    ob.setChars = setChars;
     ob.imagesReady = imagesReady;
+    ob.setFontData = setFontData;
+    ob.setCharsData = setCharsData;
     
     return ob;
 }());
