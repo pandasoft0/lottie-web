430ca54d78311e81aa1158465f1969f426f1a664
= <=>
2016-02-16 00:55:08 -0300

    new build

--- player/js/utils/expressions/ExpressionManager.js
+++ player/js/utils/expressions/ExpressionManager.js
@@ -2,18 +2,20 @@ var ExpressionManager = (function(){
     var ob = {};
 
     function sum(a,b) {
-        if(typeof a === 'number' && typeof b === 'number') {
+        var tOfA = typeof a;
+        var tOfB = typeof b;
+        if((tOfA === 'number' || tOfA === 'boolean') && (tOfB === 'number' || tOfB === 'boolean')) {
             return a + b;
         }
-        if(typeof a === 'object' && typeof b === 'number'){
+        if(tOfA === 'object' && (tOfB === 'number' || tOfB === 'boolean')){
             a[0] = a[0] + b;
             return a;
         }
-        if(typeof a === 'number' && typeof b === 'object'){
+        if((tOfA === 'number' || tOfA === 'boolean') && tOfB === 'object'){
             b[0] = a + b[0];
             return b;
         }
-        if(typeof a === 'object' && typeof b === 'object'){
+        if(tOfA === 'object' && tOfB === 'object'){
             var i = 0, lenA = a.length, lenB = b.length;
             var retArr = [];
             while(i<lenA || i < lenB){
@@ -30,18 +32,20 @@ var ExpressionManager = (function(){
     }
 
     function sub(a,b) {
-        if(typeof a === 'number' && typeof b === 'number') {
+        var tOfA = typeof a;
+        var tOfB = typeof b;
+        if((tOfA === 'number' || tOfA === 'boolean') && (tOfB === 'number' || tOfB === 'boolean')) {
             return a - b;
         }
-        if(typeof a === 'object' && typeof b === 'number'){
+        if(tOfA === 'object' && (tOfB === 'number' || tOfB === 'boolean')){
             a[0] = a[0] - b;
             return a;
         }
-        if(typeof a === 'number' && typeof b === 'object'){
+        if((tOfA === 'number' || tOfA === 'boolean') && tOfB === 'object'){
             b[0] = a - b[0];
             return b;
         }
-        if(typeof a === 'object' && typeof b === 'object'){
+        if(tOfA === 'object' && tOfB === 'object'){
             var i = 0, lenA = a.length, lenB = b.length;
             var retArr = [];
             while(i<lenA || i < lenB){
@@ -58,18 +62,20 @@ var ExpressionManager = (function(){
     }
 
     function mul(a,b) {
-        if(typeof a === 'number' && typeof b === 'number') {
+        var tOfA = typeof a;
+        var tOfB = typeof b;
+        if((tOfA === 'number' || tOfA === 'boolean') && (tOfB === 'number' || tOfB === 'boolean')) {
             return a * b;
         }
         var i, len;
-        if(typeof a === 'object' && typeof b === 'number'){
+        if(tOfA === 'object' && (tOfB === 'number' || tOfB === 'boolean')){
             len = a.length;
             for(i=0;i<len;i+=1){
                 a[i] = a[i] * b;
             }
             return a;
         }
-        if(typeof a === 'number' && typeof b === 'object'){
+        if((tOfA === 'number' || tOfA === 'boolean') && tOfB === 'object'){
             len = b.length;
             for(i=0;i<len;i+=1){
                 b[i] = a * b[i];
@@ -80,18 +86,20 @@ var ExpressionManager = (function(){
     }
 
     function div(a,b) {
-        if(typeof a === 'number' && typeof b === 'number') {
+        var tOfA = typeof a;
+        var tOfB = typeof b;
+        if((tOfA === 'number' || tOfA === 'boolean') && (tOfB === 'number' || tOfB === 'boolean')) {
             return a / b;
         }
         var i, len;
-        if(typeof a === 'object' && typeof b === 'number'){
+        if(tOfA === 'object' && (tOfB === 'number' || tOfB === 'boolean')){
             len = a.length;
             for(i=0;i<len;i+=1){
                 a[i] = a[i] / b;
             }
             return a;
         }
-        if(typeof a === 'number' && typeof b === 'object'){
+        if((tOfA === 'number' || tOfA === 'boolean') && tOfB === 'object'){
             len = b.length;
             for(i=0;i<len;i+=1){
                 b[i] = a / b[i];
@@ -134,59 +142,10 @@ var ExpressionManager = (function(){
         return Math.sqrt(addedLength);
     }
 
-    function linear(t, tMin, tMax, value1, value2){
-        if(t <= tMin) {
-            return value1;
-        }else if(t >= tMax){
-            return value2;
-        }
-        var perc = t/(tMax-tMin);
-        if(!value1.length){
-            return value1 + (value2-value1)*perc;
-        }
-        var i, len = value1.length;
-        var arr = Array.apply( null, { length: len } );
-        for(i=0;i<len;i+=1){
-            arr[i] = value1[i] + (value2[i]-value1[i])*perc;
-        }
-        return arr;
-    }
-
-    function seedRandom(){};
-    function random(min,max){
-        if(max === undefined){
-            if(min === undefined){
-                min = 0;
-                max = 1;
-            } else {
-                max = min;
-                min = undefined;
-            }
-        }
-        if(max.length){
-            var i, len = max.length;
-            if(!min){
-                min = Array.apply(null,{length:len});
-            }
-            var arr = Array.apply(null,{length:len});
-            for(i=0;i<len;i+=1){
-                arr[i] = min[i] + Math.random()*(max[i]-min[i])
-            }
-            return arr;
-        }
-        if(min === undefined){
-            min = 0;
-        }
-        return min + Math.random()*(max-min);
-    }
-
     function initiateExpression(elem,data){
         var val = data.x;
         var transform,content,effect;
         var thisComp = elem.comp;
-        elem.comp.frameDuration = 1/thisComp.globalData.frameRate;
-        var inPoint = elem.data.ip/thisComp.globalData.frameRate;
-        var outPoint = elem.data.op/thisComp.globalData.frameRate;
         var thisLayer = elem;
         var fnStr = 'var fn = function(){'+val+';this.v = $bm_rt;}';
         eval(fnStr);
@@ -419,6 +378,7 @@ var ExpressionManager = (function(){
                 this.mdf = true;
             }else{
                 if(!this.lastValue){
+
                 }
                 len = this.v.length;
                 for(i = 0; i < len; i += 1){
@@ -428,7 +388,6 @@ var ExpressionManager = (function(){
                     }
                 }
             }
-            console.log(this.v);
         }
         return execute;
     }
