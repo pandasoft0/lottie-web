f9851a676a6e606c70cca63528274d8c9b8f318e
= <=>
2017-05-29 11:29:54 +0800

    support server-side rendering use cases

--- player/js/utils/expressions/ProjectInterface.js
+++ player/js/utils/expressions/ProjectInterface.js
@@ -9,9 +9,7 @@ var ProjectInterface = (function (){
             var i = 0, len = this.compositions.length;
             while(i<len){
                 if(this.compositions[i].data && this.compositions[i].data.nm === name){
-                    if(this.compositions[i].prepareFrame) {
-                        this.compositions[i].prepareFrame(this.currentFrame);
-                    }
+                    this.compositions[i].prepareFrame(this.currentFrame);
                     return this.compositions[i].compInterface;
                 }
                 i+=1;
