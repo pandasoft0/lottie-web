cf63022bd7d8dceeaf06830493430dca9cd416cf
= <=>
2018-03-15 00:04:59 +0000

    Add another [easy] install option

--- build/player/lottie_light.js
+++ build/player/lottie_light.js
@@ -1,4 +1,4 @@
-(typeof navigator !== "undefined") && (function(root, factory) {
+(function(root, factory) {
     if (typeof define === "function" && define.amd) {
         define(function() {
             return factory(root);
